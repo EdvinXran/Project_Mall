e8f88ce3dc05059fc1d2b61162aa1489fad2bdb9
tangxuan <seizeday@yeah.net>
2019-05-22 19:20:13 +0800

    项目框架

---
+++ mall/src/main/java/com/cskaoyan/mall/controller/product/Example.java
@@ -0,0 +1,8 @@
+package com.cskaoyan.mall.controller.product;
+
+/**
+ * @Author: Tangxuan
+ * @Date: 2019/5/22 19:18
+ */
+public class Example {
+}
---
+++ mall/src/main/java/com/cskaoyan/mall/vo/Example.java
@@ -0,0 +1,8 @@
+package com.cskaoyan.mall.vo;
+
+/**
+ * @Author: Tangxuan
+ * @Date: 2019/5/22 19:18
+ */
+public class Example {
+}
